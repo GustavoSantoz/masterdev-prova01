59187177af4f762fe025769f7280a52b305f0d7c
GustavoSantoz <Guga_Vine@outlook.com>
2024-02-08 23:42:56 -0300

    if and switch

--- src/marminino.js
+++ src/marminino.js
@@ -1,8 +1,16 @@
-function executar(numero) {
-    return "";
-    //return "Mar";
-    //return "Minino"
-    //return "MarMinino";
+function executar2(numero) {
+    switch (true) {
+        case numero % 3 === 0 && numero % 4 === 0:
+            return "MarMinino";
+        case numero % 3 === 0:
+            return "Mar";
+        case numero % 4 === 0:
+            return "Minino";
+        default:
+            return "";
+    }
 }
-
-module.exports = executar;
+for(i = 1; i <= 100; i++) {
+    console.log(i + ": " + executar2(i))
+}
+module.exports = executar2;
---
+++ src/marminino2.js
@@ -0,0 +1,16 @@
+function executar(numero) {
+    resultado = ""
+    if (numero % 3 === 0) {
+        resultado += "Mar"
+    }
+    if (numero % 4 === 0) {
+        resultado += "Minino"
+    }
+    return resultado
+}
+
+for (i = 0; i++; i <= 100) {
+    executar(i + ": " + Resultado(i))
+}
+
+module.exports = executar;
